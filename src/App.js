47a5fdfbde29c92c0fd4d621e3f56c85d542326d
Shtrubin <rubeensht17@gmail.com>
2024-01-19 21:17:24 +0545

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import WeatherApp from './components/Weatherapp/WeatherApp';
+// import WeatherApp from './components/weatherapp/weatherApp';
+
+function App() {
+  return (
+    <div className="App">
+      <WeatherApp/>
+    </div>
+  );
+}
+
+export default App;
